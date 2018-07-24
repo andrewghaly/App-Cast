41c516f8483270ee5be762a16286c3f8b27ed74a
Andrew Ghaly <andrewmghaly@gmail.com>
2018-07-24 00:43:17 -0400

    Proof of concept: control chromecast media with wireless enabled device

--- application.py
+++ application.py
@@ -1,13 +1,61 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from threading import Thread, Event
+from time import sleep
+from werkzeug.contrib.cache import SimpleCache
 
-application = Flask(__name__)
-application.debug = False
+cache = SimpleCache()
+
+__author__ = 'andrew_ghaly'
+
+app = Flask(__name__)
+app.debug = False
+sock = SocketIO(app)
+
+thread = Thread()
+cache.set("current-action", "play")
 
 
-@application.route("/", methods=['GET', 'POST'])
-def main():
-    return render_template('index.html')
+class ControlThread(Thread):
+    def __init__(self):
+        self.delay = 3
+        super(ControlThread, self).__init__()
+
+    def main(self):
+        while True:
+            current_action = cache.get("current-action")
+            sock.emit('control-update', {
+                      'message': current_action}, namespace='/control')
+            sleep(self.delay)
+
+    def run(self):
+        self.main()
 
 
-if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=25200)
+@app.route('/')
+def index():
+    return render_template('video.html')
+
+
+@app.route('/play')
+def play_command():
+    cache.set("current-action", "play")
+    return "play command"
+
+
+@app.route('/pause')
+def pause_command():
+    cache.set("current-action", "pause")
+    return "pause command"
+
+
+@sock.on('connect', namespace='/control')
+def connect():
+    global thread
+    if not thread.isAlive():
+        thread = ControlThread()
+        thread.start()
+
+
+if __name__ == '__main__':
+    sock.run(app, host='0.0.0.0', port=25200)
--- static/scripts/source.js
+++ static/scripts/source.js
@@ -1,3 +1,21 @@
-$(function(){
-    console.log("hello, console!");
+$(function () {
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/control');
+
+    var videoContents = function (event) {
+        var videoPlayer = document.querySelector('video')
+        var file = this.files[0]
+        var fileURL = URL.createObjectURL(file)
+        videoPlayer.src = fileURL
+    }
+
+    var videoInput = document.querySelector('input')
+    videoInput.addEventListener('change', videoContents, false)
+
+    socket.on('control-update', function (msg) {
+        if (msg.message == 'pause') {
+            $('#cast-video').get(0).pause();
+        } else if (msg.message == 'play') {
+            $('#cast-video').get(0).play();
+        }
+    });
 });
